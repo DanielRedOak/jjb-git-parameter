87691a7430060564cb05ea453ae23c25f95830bf
Ryan O'Keeffe <okeefferd@gmail.com>
2016-05-27 11:48:48 -0500

    wip

---
+++ jjb_git_parameter/git.py
@@ -0,0 +1,40 @@
+import xml.etree.ElementTree as XML
+
+def git_parameter(parser, xml_parent, data):
+  """yaml: git
+
+  Example::
+    parameters:
+      - git:
+         name:
+         description:
+         parameter-type:
+         branch:
+         branch-filter:
+         tag-filter:
+         sort-mode:
+         default-value:
+         quick-filter:
+  """
+  if data is None:
+    data = dict()
+
+  notifier = XML.SubElement(xml_parent, 'net.uaznia.lukanus.hudson.plugins.gitparameter.GitParameterDefinition')
+  notifier.set('plugin', 'git-parameter@0.5.1')
+
+  for opt, attr in (
+            ('name', 'name'),
+            ('description', 'description'),
+            ('parameter-type', 'type'),
+            ('branch', 'branch'),
+            ('branch-filter', 'branchFilter'),
+            ('tag-filter', 'tagFilter'),
+            ('sort-mode', 'sortMode'),
+            ('default-value', 'defaultValue'),
+            ('quick-filter', 'quickFilterEnabled')
+   ):
+     value = data.get(opt, '')
+     if isinstance(notifier, str):
+       XML.SubElement(data, attr).text = value
+     else:
+      XML.SubElement(notifier, attr).text = str(value).lower()
